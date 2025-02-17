40f184a81877d8313fbd4c4db7cab3dd99d19b0a
EryckBorges <eryckborges2502@gmail.com>
2025-02-17 10:24:10 -0300

    Responsivisei a pokedex

--- projects/pokemon/pokedex/js/script.js
+++ projects/pokemon/pokedex/js/script.js
@@ -1,80 +1,98 @@
 const app = {
     atual: 0,
+    permitirSom: false, // Som só toca após interação do usuário
+
     init: function() {
-        //Obtem os elementos html
+        // Obtém os elementos HTML
         this.titulo = document.querySelector(".tit");
         let prev = document.querySelector("#prev");
         this.dad = document.querySelector(".tela");
         this.pokedad = document.querySelector(".pokemon-dados");
         let next = document.querySelector("#next");
         this.imagem = document.querySelector(".pokemon-img");
-        //Atribui eventos aos botões
-        prev.onclick = ( ) => {
-            this.showPokemon(this.atual-1);
+
+        // Atribui eventos aos botões
+        prev.onclick = () => {
+            this.showPokemon(this.atual - 1);
         };
-        next.onclick = ( ) => {
-            this.showPokemon(this.atual+1);
+        next.onclick = () => {
+            this.showPokemon(this.atual + 1);
         };
 
         this.som = null;
-        //Exibe o pokemon atual
+
+        // Ativar som após interação do usuário
+        document.addEventListener("click", () => {
+            app.permitirSom = true;
+        }, { once: true });
+
+        // Exibe o Pokémon atual
         this.showPokemon(this.atual);
     },
 
-    showPokemon:function(atual){
-        if(atual < 0) atual = dados.length - 1;
-        if(atual === dados.length) atual = 0;
+    showPokemon: function(atual) {
+        if (atual < 0) atual = dados.length - 1;
+        if (atual === dados.length) atual = 0;
 
         this.atual = atual;
         let pokemon = dados[atual];
-        //Emite som
-        if(pokemon.som !== undefined) {
-            if(this.som !== null) {
+
+        // Emite som apenas se o usuário já tiver interagido
+        if (pokemon.som !== undefined && this.permitirSom) {
+            if (this.som !== null && !this.som.paused) {
                 this.som.pause();
             }
             this.som = new Audio(pokemon.som);
-            this.som.play();
-        };
-        //Seleciona o Nome
-        let nome = `<big>${pokemon.nro} - ${pokemon.nome}${pokemon.genero}<big>`;
-        //Seleciona a Imagem
-        let img = `<img class="pok-img${pokemon.tamanho}" src="${pokemon.img}"/>`;
-        //Seleciona a Descrição
+            this.som.play().catch(err => console.warn("Erro ao tentar reproduzir áudio:", err));
+        }
+
+        // Seleciona o Nome
+        let nome = `<big>${pokemon.nro} - ${pokemon.nome}${pokemon.genero}</big>`;
+
+        // Seleciona a Imagem
+        let img = `<img class="pok-img${pokemon.tamanho}" src="${pokemon.img}" alt="${pokemon.nome}"/>`;
+
+        // Seleciona a Descrição
         let desc = "";
         let tit = " ";
-        for(let tipo of pokemon.tipo) tit += `<img class='icon' src="${tipos[tipo]}">${tipo.toUpperCase()} `;
+        for (let tipo of pokemon.tipo) {
+            tit += `<img class='icon' src="${tipos[tipo]}" alt="${tipo}"> ${tipo.toUpperCase()} `;
+        }
         desc += `<br/>${pokemon.descricao}`;
-        //Exibe a Descrição do Pokémon
+
+        // Exibe a Descrição do Pokémon
         this.dad.innerHTML = desc;
-        //Exibe a Imagem do Pokémon
+
+        // Exibe a Imagem do Pokémon
         this.imagem.innerHTML = img;
-        //Exibe o Nome e o Número
+
+        // Exibe o Nome e o Número
         this.pokedad.innerHTML = nome;
         this.titulo.innerHTML = tit;
     },
 
-    onSearch: function(event){
+    onSearch: function(event) {
         let pokeFind = event.target.value;
-        if(Number.isNaN(parseInt(pokeFind)) && pokeFind.length > 3){
-            for(let i=0; i<dados.length; i++){
+        if (Number.isNaN(parseInt(pokeFind)) && pokeFind.length > 3) {
+            for (let i = 0; i < dados.length; i++) {
                 let p = dados[i];
-                if(p.nome.toLowerCase().includes(pokeFind.toLowerCase())){
+                if (p.nome.toLowerCase().includes(pokeFind.toLowerCase())) {
                     this.showPokemon(i);
-                    i = dados.length;
+                    break;
                 }
             }
-        } else if(!Number.isNaN(parseInt(pokeFind))){
-            for(let i=0; i<dados.length; i++){
+        } else if (!Number.isNaN(parseInt(pokeFind))) {
+            for (let i = 0; i < dados.length; i++) {
                 let p = dados[i];
-                if(p.nro === parseInt(pokeFind)){
+                if (p.nro === parseInt(pokeFind)) {
                     this.showPokemon(i);
-                    i = dados.length;
+                    break;
                 }
             }
         }
-
     }
 };
-window.onload = function(){
+
+window.onload = function() {
     app.init();
 };
