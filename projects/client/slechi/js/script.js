b077a19e72911d4d3011ecce1ea6ad014513d305
EryckBorges <eryckborges2502@gmail.com>
2025-02-21 09:47:10 -0300

    Responsivisei a animação do header e toda a tela slechi

--- projects/client/slechi/js/script.js
+++ projects/client/slechi/js/script.js
@@ -9,6 +9,7 @@ const eua = {
   use: 'Terms of Use',
   cookie: 'Cookie Policy',
   privacity: 'Privacy',
+  slechiText: 'In this project, I developed card layouts and menus for Slechi ice cream shop, aiming to provide an attractive and intuitive visual experience for customers. The designs were created to clearly and neatly showcase all types of available products, highlighting each flavor in a practical and accessible way. Each card was designed to be visually appealing, making it easier for customers to choose and enhancing the overall menu navigation experience.',
 }
 
 const brasil = {
@@ -22,6 +23,7 @@ const brasil = {
     use: 'Termos de Uso',
     cookie: 'Politica de Cookies',
     privacity: 'Privacidade',
+    slechiText: 'Nesse projeto, desenvolvi layouts de cards e cardápios para a sorveteria Slechi, com o objetivo de oferecer uma experiência visual atrativa e intuitiva para os clientes. Os designs foram pensados para apresentar de forma clara e organizada todos os tipos de produtos disponíveis, destacando cada sabor de maneira prática e acessível. Cada card foi projetado para ser visualmente agradável, facilitando a escolha do cliente e tornando o processo de navegação pelo cardápio mais simples e envolvente.',
 }
 
 const spain = {
@@ -35,6 +37,7 @@ const spain = {
   use: 'Términos de Uso',
   cookie: 'Política de Cookies',
   privacity: 'Privacidad',
+  slechiText: 'En este proyecto, desarrollé diseños de tarjetas y menús para la heladería Slechi, con el objetivo de ofrecer una experiencia visual atractiva e intuitiva para los clientes. Los diseños fueron creados para mostrar de manera clara y organizada todos los tipos de productos disponibles, destacando cada sabor de forma práctica y accesible. Cada tarjeta fue diseñada para ser visualmente atractiva, facilitando la elección del cliente y mejorando la experiencia general de navegación por el menú.',
 }
 
 const franca = {
@@ -48,6 +51,7 @@ const franca = {
   use: 'Conditions d’utilisation',
   cookie: 'Politique de Cookies',
   privacity: 'Confidentialité',
+  slechiText: "Dans ce projet, j'ai développé des mises en page de cartes et de menus pour la glacerie Slechi, dans le but d'offrir une expérience visuelle attrayante et intuitive aux clients. Les designs ont été créés pour présenter clairement et de manière organisée tous les types de produits disponibles, en mettant en avant chaque saveur de façon pratique et accessible. Chaque carte a été conçue pour être visuellement attrayante, facilitant le choix des clients et améliorant l'expérience globale de navigation dans le menu.",
 }
 
 const china = {
@@ -61,6 +65,7 @@ const china = {
   use: '使用条款',
   cookie: 'Cookie 政策',
   privacity: '隐私政策',
+  slechiText: '在这个项目中，我为 Slechi 冰淇淋店设计了卡片和菜单布局，旨在为顾客提供直观且吸引人的视觉体验。设计旨在清晰、有条理地展示所有可用产品类型，并以实用且易于访问的方式突出每种口味。每张卡片都经过精心设计，具有视觉吸引力，方便顾客选择，提升整体菜单浏览体验。',
 }
 
 const elementos = {
@@ -74,6 +79,7 @@ const elementos = {
     use: document.querySelector('.use'),
     cookie: document.querySelector('.cookie'),
     privacity: document.querySelector('.privacity'),
+    slechiText: document.querySelector('.textProject p'),
 }
 
 const ingles = () => {
@@ -99,6 +105,7 @@ const ingles = () => {
     elementos.use.innerHTML = eua.use;
     elementos.privacity.innerHTML = eua.privacity;
     elementos.cookie.innerHTML = eua.cookie;
+    elementos.slechiText.innerHTML = eua.slechiText;
 }
 
 const portugues = () => {
@@ -124,6 +131,7 @@ const portugues = () => {
   elementos.use.innerHTML = brasil.use;
   elementos.privacity.innerHTML = brasil.privacity;
   elementos.cookie.innerHTML = brasil.cookie;
+  elementos.slechiText.innerHTML = brasil.slechiText;
 }
 
 const espanha = () => {
@@ -149,6 +157,7 @@ const espanha = () => {
     elementos.use.innerHTML = spain.use;
     elementos.privacity.innerHTML = spain.privacity;
     elementos.cookie.innerHTML = spain.cookie;
+    elementos.slechiText.innerHTML = spain.slechiText;
 }
 
 const frances = () => {
@@ -174,6 +183,7 @@ const frances = () => {
     elementos.use.innerHTML = franca.use;
     elementos.privacity.innerHTML = franca.privacity;
     elementos.cookie.innerHTML = franca.cookie;
+    elementos.slechiText.innerHTML = franca.slechiText;
 }
 
 const chines = () => {
@@ -199,6 +209,7 @@ const chines = () => {
     elementos.use.innerHTML = china.use;
     elementos.privacity.innerHTML = china.privacity;
     elementos.cookie.innerHTML = china.cookie;
+    elementos.slechiText.innerHTML = china.slechiText;
 }
 
 
@@ -296,3 +307,94 @@ document.addEventListener("DOMContentLoaded", () => {
         slider.scrollLeft = scrollLeft - walk;
     });
 });
+
+const sabores = document.querySelector('.sabores');
+
+const grantop = document.querySelector('.grantop');
+const sorveteGrantop = document.querySelector('.sorveteGrantop');
+const leite = document.querySelector('.leite');
+const morangoBottom = document.querySelector('.morangoBottom');
+const morangoTop = document.querySelector('.morangoTop');
+const morangoBack = document.querySelector('.morangoBack');
+const morangoFront = document.querySelector('.morangoFront');
+
+const ituzinho = document.querySelector('.ituzinho');
+const sorveteItuzinho = document.querySelector('.sorveteItuzinho');
+const morangoBottom2 = document.querySelector('.morangoBottom2');
+const morangoTop2 = document.querySelector('.morangoTop2');
+const morangoBack2 = document.querySelector('.morangoBack2');
+const morangoFront2 = document.querySelector('.morangoFront2');
+
+const acai = document.querySelector('.acai');
+const acai1 = document.querySelector('.acai1');
+const acai2 = document.querySelector('.acai2');
+const acai3 = document.querySelector('.acai3');
+const acaiCaindo = document.querySelector('.acaiCaindo');
+
+const beijinho = document.querySelector('.beijinho');
+const sorveteB = document.querySelector('.sorveteB');
+const beijinhoBottom = document.querySelector('.beijinhoBottom');
+const beijinhoTop = document.querySelector('.beijinhoTop');
+const beijinhoFront = document.querySelector('.beijinhoFront');
+const beijinhoBack = document.querySelector('.beijinhoBack');
+const leite2 = document.querySelector('.leite2');
+
+const animateHeader = () => {
+  setTimeout(() => {
+    morangoBottom.style.animation = "fadeOutDown 0.5s";
+    morangoFront.style.animation = "fadeOutDown 0.5s";
+    morangoBack.style.animation = "fadeOutLeft 0.5s";
+    morangoTop.style.animation = "fadeOutUp 0.5s";
+    leite.style.animation = "zoomOut 1s";
+    sorveteGrantop.style.animation = "fadeOutDown 0.5s";
+    setTimeout(() => {
+      grantop.classList.add('opacity')
+   
+        grantop.style.display = 'none';
+      sabores.style.background = 'var(--linearItuzinho)';
+      ituzinho.style.display = 'flex';
+      
+      setTimeout(() => {
+        
+        sorveteItuzinho.style.animation = "fadeOutDownBig 0.5s";
+        morangoBottom2 .style.animation = "fadeOutDown 0.5s";
+        morangoFront2 .style.animation = "fadeOutDown 0.5s";
+        morangoBack2 .style.animation = "fadeOutLeft 0.5s";
+        morangoTop2 .style.animation = "fadeOutUp 0.5s";
+        setTimeout(() => {
+          
+          ituzinho.style.display = 'none';
+          sabores.style.background = 'var(--linearAcai)';
+          acai.style.display = 'flex';
+          setTimeout(() => {
+            acaiCaindo.style.animation = "fadeOutDownBig 0.5s";
+            acai1.style.animation = "fadeOutDownBig 0.5s";
+            acai2.style.animation = "fadeOutDownBig 0.5s";
+            acai3.style.animation = "fadeOutDownBig 0.5s";
+            setTimeout(() => {
+              acai.style.display = 'none';
+              sabores.style.background = 'var(--linearBeijinho)';
+              beijinho.style.display = 'flex';
+              setTimeout(() => {
+                beijinhoBottom.style.animation = "fadeOutDownBig 0.5s";
+                leite2.style.animation = "zoomOut 0.5s";
+                sorveteB.style.animation = "fadeOutDownBig 0.5s";
+                beijinhoFront.style.animation = "fadeOutDownBig 0.5s";
+                beijinhoBack.style.animation = "fadeOutDownBig 0.5s";
+                beijinhoTop.style.animation = "fadeOutDownBig 0.5s";
+                setTimeout(() => {
+                  beijinho.style.display = 'none';
+                  sabores.style.background = 'var(--linearMorango)';
+                  grantop.style.display = 'flex';
+                  animateHeader();
+                }, 500);
+              }, 5000);
+            }, 500);
+          }, 5000);
+        }, 500);
+      }, 5000);
+    }, 500);
+  }, 5000);
+}
+
+animateHeader();
