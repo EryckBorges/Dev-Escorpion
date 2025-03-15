ed9a04f11beea7e833310ed4d5741d3d510355d0
eryckborges <eryckborges2502@gmail.com>
2025-03-15 17:14:15 -0300

    Responsivisei o  header da tela de genshin Impact

---
+++ projects/genshinImpact/js/script.js
@@ -0,0 +1,35 @@
+const playStore = document.querySelector('.playStore');
+
+playStore.addEventListener('click', () => {
+    window.location.href = 'https://play.google.com/store/apps/details?id=com.miHoYo.GenshinImpact&pli=1xx';
+})
+
+const xbox = document.querySelector('.xbox');
+
+xbox.addEventListener('click', () => {
+    window.location.href = 'https://www.xbox.com/pt-BR/games/store/genshin-impact/9N7TFFRRZCC9';
+})
+
+const ps = document.querySelector('.ps');
+
+ps.addEventListener('click', () => {
+    window.location.href = 'https://www.playstation.com/pt-br/games/genshin-impact/';
+})
+
+const epic = document.querySelector('.epic');
+
+epic.addEventListener('click', () => {
+    window.location.href = 'https://store.epicgames.com/pt-BR/p/genshin-impact';
+})
+
+const apple = document.querySelector('.apple');
+
+apple.addEventListener('click', () => {
+    window.location.href = 'https://apps.apple.com/us/app/genshin-impact/id1517783697';
+})
+
+const pc = document.querySelector('.pc');
+
+pc.addEventListener('click', () => {
+    window.location.href = 'https://genshin.hoyoverse.com/en/home';
+})
