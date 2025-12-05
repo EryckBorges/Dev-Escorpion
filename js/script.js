f45af691124e86437ffd5d047cef97797e61ca93
= <=>
2025-12-05 09:59:08 -0300

    Adicionei o meta ads

--- js/script.js
+++ js/script.js
@@ -3,6 +3,19 @@ import { ToastFunction, MostrarImagens } from "../../global/js/script.js";
 
 // Assim que carregar a página já cria um toast e adiciona a própria página para poder ser manipulado pelo DOM
 document.addEventListener('DOMContentLoaded', () => {
+  
+  // <!-- Meta Pixel Code -->
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '25174390548887635');
+fbq('track', 'PageView');
+
   // Cria a caixa para exibição de imagens e outros elementos
   const boxImages = new MostrarImagens();
     
